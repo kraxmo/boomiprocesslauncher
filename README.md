d1be3319b7f4d8ce8e78765f8e347bc098bd8a22
Jim Kraxberger <jkraxberger@schoolsfirstfcu.org>
2025-06-13 07:20:54 -0700

    modified to add api and user credentials to command line

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
-# boomiprocesslauncher
-This pythonic script runs a Boomi integration process via Boomi API call.
+# boomi_process_launcher.py
+This script runs a Boomi integration process via Boomi API call.
 
-It does the following:
+It verifies the following:
+1. api_url
+2. path_url
+3. username
+4. password
+5. Valid atom name
+6. Valid atom environment
+7. Valid process name
+8. Valid process deployed in atom environment
+
+It executes the process and verifies it has been queued for execution
+
+If optional -w or -wait parameter is specified, script will attempt to wait for process to complete (successful or fail)
+
+# test_boomi_process_launcher.py
+This script runs a unittest vs. boomi_process_launcher.py
+
+# boomi_process_launcher_from_config.py
+This script runs a Boomi integration process via Boomi API call using a configuration file.
+
+It verifies the following:
 1. Valid atom name
 2. Valid atom environment
 3. Valid process name
 4. Valid process deployed in atom environment
-5. If optional -w or -wait parameter is specified, 
-   script will attempt to wait for process to complete (successful or fail)
 
-UNIT TESTING:
+It executes the process and verifies it has been queued for execution
 
-test_boomi_process_launcher.py mock tests all API calls *without* communicating with Boomi Atom API layer:
+If optional -w or -wait parameter is specified, script will attempt to wait for process to complete (successful or fail)
 
->py -m unittest test_boomi_process_launcher.py
+# test_boomi_process_launcher_with_config.py
+This script runs a unittest vs. boomi_process_launcher_with_config.py
+
+*Known issue*
+class TestPrintLogMessage function test_format_log_message returns errors even though when calling the same function within test_boomi_process_launcher.py executes successfully.
--- test_boomi_process_launcher.py
+++ test_boomi_process_launcher.py
@@ -19,7 +19,7 @@ def identify(func):
 
 class TestUtility(ut1.TestCase):
     def setUp(self):
-        self.boomi = BoomiAPI("atom_name", "process_name", False, None, False)
+        self.boomi = BoomiAPI("api_url", "path_url", "username", "password", "atom_name", "process_name", False, None, False)
 
     @identify
     def test_convert_from_iso_to_local(self):
@@ -39,27 +39,26 @@ class TestUtility(ut1.TestCase):
     @identify
     def test_parse_dynamic_properties_valid(self):
         # test parsing validly formatted dynamic process properties
-        self.boomi = BoomiAPI("atom_name", "process_name", False, "key1:value1;key2:value2", True)
+        self.boomi = BoomiAPI("api_url", "path_url", "username", "password", "atom_name", "process_name", False, "key1:value1;key2:value2", True)
         self.boomi.parse_dynamic_properties()
 
     @identify
     def test_parse_dynamic_properties_invalid(self):
         # test parsing invalidly formatted dynamic process properties
-        self.boomi = BoomiAPI("atom_name", "process_name", False, "key1:value1;key2", True)
+        self.boomi = BoomiAPI("api_url", "path_url", "username", "password", "atom_name", "process_name", False, "key1:value1;key2", True)
         with self.assertRaises(ScriptExitException):
             self.boomi.parse_dynamic_properties()
 
 class TestPrintLogMessage(ut1.TestCase):
     def setUp(self):
-        self.boomi = BoomiAPI("atom_name", "process_name", False, None, False)
+        self.boomi = BoomiAPI("api_url", "path_url", "username", "password", "atom_name", "process_name", False, None, False)
 
     @patch('datetime.datetime', autospec=True)
     @patch('boomi_process_launcher.datetime', autospec=True)
     @identify
     def test_format_log_message(self, mock_datetime, mock_boomi_api):
         # test various formatting combinations of log message formatter
-        mock_datetime.now.return_value = datetime.datetime.strptime("2024-01-01 08:01:02", "%Y-%m-%d %H:%M:%S")
-        mock_boomi_api.now.return_value = datetime.datetime.strptime("2024-01-01 08:01:02", "%Y-%m-%d %H:%M:%S")
+        mock_boomi_api.now.return_value = mock_datetime.now.return_value = datetime.datetime.strptime("2024-01-01 08:01:02", "%Y-%m-%d %H:%M:%S")
         groups = [
             ('1only', '12345', None, None, None),
             ('1only', '12345678901234567890123456789012345', None, None, None),
@@ -82,20 +81,9 @@ class TestPrintLogMessage(ut1.TestCase):
                 elif lines == '4':
                     self.assertEqual(f"{datetime.datetime.now()}\t{group1.ljust(self.boomi.GROUP1_LENGTH)}{group2}\n\t\t\t\t{group3}\n\t\t\t\t{group4}", log)
 
-class TestConfigFile(ut1.TestCase):
-    def setUp(self):
-        self.boomi = BoomiAPI("atom_name", "process_name", False, None, False)
-        self.python_script_path = ''
-
-    @identify
-    def test_retrieve_api_settings(self):
-        # test retrieving api settings from external config (needs to read Mock file instead)
-        self.boomi.retrieve_api_settings()
-        self.assertEqual("api.boomi.com", self.boomi.api_url)
-
 class TestRequestResponse(ut1.TestCase):
     def setUp(self):
-        self.boomi = BoomiAPI("atom_name", "process_name", False, None, False)
+        self.boomi = BoomiAPI("api_url", "path_url", "username", "password", "atom_name", "process_name", False, None, False)
         self.boomi.api_url = "jsonplaceholder.typicode.com"
         self.boomi.path_url = "/posts"
         self.boomi.username = "username"
